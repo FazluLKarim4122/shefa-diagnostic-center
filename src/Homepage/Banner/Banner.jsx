498b72ad4d9235672524573f90c87c6fa915ec05
FazluLKarim4122 <md.fazlulkarim4122@gmail.com>
2024-12-10 19:01:21 +0600

    added link to all pages in the button

--- src/Homepage/Banner/Banner.jsx
+++ src/Homepage/Banner/Banner.jsx
@@ -5,6 +5,7 @@ import { Carousel } from 'react-responsive-carousel';
 import img1 from '../../assets/banner1.jpg';
 import img2 from '../../assets/banner2.jpg';
 import img3 from '../../assets/banner3.jpg';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
@@ -12,15 +13,21 @@ const Banner = () => {
             <Carousel>
                     <div className='relative'>
                         <img src={img1} alt="" />
+                        <Link to='/allTests'>
                         <button className='btn btn-accent absolute left-4 bottom-10 '>All Tests</button>
+                        </Link> 
                     </div>
                     <div className='relative'>
                         <img src={img2} alt="" />
+                        <Link to='/allTests'>
                         <button className='btn btn-accent absolute left-4 bottom-10 '>All Tests</button>
+                        </Link> 
                     </div>
                     <div className='relative'>
                         <img src={img3} alt="" />
+                        <Link to='/allTests'>
                         <button className='btn btn-accent absolute left-4 bottom-10 '>All Tests</button>
+                        </Link> 
                     </div>
             </Carousel>
         </div>
