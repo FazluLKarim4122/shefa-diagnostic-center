bb324d21dbb13b399cbd216fdcfd655f6588b181
FazluLKarim4122 <md.fazlulkarim4122@gmail.com>
2024-12-10 22:54:03 +0600

    add firebase api keys

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -4,16 +4,18 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+console.log(import.meta.env.VITE_APIKEY);
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDUvwpNniBNKsbFKW7dPJIZhW82q6v9aBI",
-  authDomain: "shefa-diagnostic.firebaseapp.com",
-  projectId: "shefa-diagnostic",
-  storageBucket: "shefa-diagnostic.firebasestorage.app",
-  messagingSenderId: "303911833376",
-  appId: "1:303911833376:web:e33a3819d57ea8450a5145",
-  measurementId: "G-JGTFQSF4RS"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
 // Initialize Firebase
