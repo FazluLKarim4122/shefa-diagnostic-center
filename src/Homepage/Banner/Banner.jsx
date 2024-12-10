bbd31f37f55ac5d463329c1baa7d0890edd8ca41
FazluLKarim4122 <md.fazlulkarim4122@gmail.com>
2024-12-10 18:49:52 +0600

    banner setup

--- src/Homepage/Banner/Banner.jsx
+++ src/Homepage/Banner/Banner.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+import img1 from '../../assets/banner1.jpg';
+import img2 from '../../assets/banner2.jpg';
+import img3 from '../../assets/banner3.jpg';
 
 const Banner = () => {
     return (
         <div>
-            
+            <Carousel>
+                    <div>
+                        <img src={img1} alt="" />
+                    </div>
+                    <div >
+                        <img src={img2} alt="" />
+                    </div>
+                    <div>
+                        <img src={img3} alt="" />
+                    </div>
+            </Carousel>
         </div>
     );
 };
